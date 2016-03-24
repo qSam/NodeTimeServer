0fefe03dcedccf5b25e6e590ca268e025339db06
Qasim Laghari <go.jets.laghari@gmail.com>
2016-03-24 13:39:04 -0500

    Update with latest JSON API Test

--- program.js
+++ program.js
@@ -4,10 +4,57 @@ var myModule = require ('./myModule.js')
 var http = require ('http');
 var bl = require ('bl');
 var net = require('net');
-var strftime = require('strftime')
+var strftime = require('strftime');
+var map = require('through2-map');
+var url = require('url');
 
 
 var port = process.argv[2];
+var filePath = process.argv[3];
+
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200, { 'content-type': 'text/json' });
+
+    var urlStore = url.parse(req.url, true);
+    //console.log(urlStore);
+
+    if (req.method === 'GET'){
+        if(urlStore.pathname === '/api/parsetime') {
+          //console.log(urlStore.query.iso);
+          var d = new Date(urlStore.query.iso);
+          var hours = d.getHours();
+          var min  = d.getMinutes();
+          var sec = d.getSeconds();
+          //console.log(hours + ', ' + min + ', ' + sec)
+          res.end(JSON.stringify({
+            hour: hours,
+            minute: min,
+            second: sec
+          }));
+
+        }
+
+        if(urlStore.pathname ===  '/api/unixtime') {
+          var d = new Date(urlStore.query.iso);
+          var unixT = (d.getTime());
+
+          res.end(JSON.stringify({
+            unixtime: unixT
+          }));
+
+        }
+    }
+
+
+});
+
+server.listen(port);
+
+
+
+/*
+Create TCP Server
 
 var server = net.createServer(function (socket) {
   // socket handling logic
@@ -17,7 +64,7 @@ var server = net.createServer(function (socket) {
 });
 server.listen(port);
 
-
+*/
 
 
 /*
